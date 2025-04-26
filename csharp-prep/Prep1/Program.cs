2b36c04c0c071c135f9bee20599623e95456f4f1
Cameron Woodward <woo23017@byui.edu>
2025-04-26 12:28:22 -0600

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,45 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
 
-          int x = 0;
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
 
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine($"Your name is {last}, {first} {last}");
 
-        Console.WriteLine("Please enter a number");
-        string userInput = Console.ReadLine();
-        int userInputAsInt = int.Parse(userInput);
-
-        int y=10;
-        int asdf=0;
-        asdf=10;
-        Console.WriteLine(asdf);
-
-        if (userInputAsInt > 0) 
-        {
-            Console.WriteLine($"This number {userInputAsInt} is greater than 0");
-
-               // do nothing
-        } else if (userInputAsInt < 0) 
-        {
-            Console.WriteLine($"This number {userInputAsInt} is less than 0");
-
-        }
-        else if (userInputAsInt == 0)
-        {
-            Console.WriteLine($"This number {userInputAsInt} is 0");
-
-        }
-        if (userInputAsInt>0 || y==10) 
-        {
-
-        }
-        if (userInputAsInt>0) {
-
-        }
-        if (y == 10) {
-
-        }
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+         int x = 0;
+
+        Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("Please enter a number");
+        string userInput = Console.ReadLine();
+        int userInputAsInt = int.Parse(userInput);
+
+        int y=10;
+        int asdf=0;
+        asdf=10;
+        Console.WriteLine(asdf);
+
+        if (userInputAsInt > 0) 
+        {
+            Console.WriteLine($"This number {userInputAsInt} is greater than 0");
+
+               // do nothing
+        } else if (userInputAsInt < 0) 
+        {
+            Console.WriteLine($"This number {userInputAsInt} is less than 0");
+
+        }
+        else if (userInputAsInt == 0)
+        {
+            Console.WriteLine($"This number {userInputAsInt} is 0");
+
+        }
+        if (userInputAsInt>0 || y==10) 
+        {
+
+        }
+        if (userInputAsInt>0) {
+
+        }
+        if (y == 10) {
+
+        }
+ 
     }
 }
